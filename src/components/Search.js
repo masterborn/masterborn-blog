f0671ee0fcb1692cbe30121f568b05577e38d760
Jakub Bosak <jBosak98@gmail.com>
2021-04-14 17:44:33 +0200

    add visibility toggle - searchComponent

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,37 +5,49 @@ import { connectSearchBox } from "react-instantsearch-dom"
 
 import { ReactComponent as SearchIcon } from '../assets/search.svg';
 
-
-
 const StyledInput = styled.input`
   border: 0px;
   margin-left: auto;
   margin-right: 1rem;
   min-width: 19rem;
 `;
+
 const StyledForm = styled.form`
   margin-left: auto;
+  min-width: 19rem;
+  display: grid;
+  grid-auto-flow: column;
 `;
+
 const StyledIcon = styled(SearchIcon)`
   cursor: pointer;
+  margin-left: auto;
+  margin-right: 0;
 `;
 
 const Search = ({ refine, onFocus }) => {
   const [keyword, setKeyword] = useState(null);
+  const [visibility, setVisibility] = useState(false);
   const onSubmit = (event) => {
     event.preventDefault();
     refine(keyword);
   };
   return (
-    <StyledForm onSubmit={onSubmit}>
-      <StyledInput 
-        type="text"
-        value={keyword}
-        placeholder="|Search article by name..."
-        aria-label="Search"
-        onChange={event => setKeyword(event.target.value)}
-        onFocus={onFocus}
-      />
+    <StyledForm
+      onSubmit={onSubmit} 
+      onMouseEnter={()=>setVisibility(true)}
+      onMouseLeave={()=>setVisibility(false)}
+    >
+      {(keyword || visibility) && (
+        <StyledInput 
+          type="text"
+          value={keyword}
+          placeholder="|Search article by name..."
+          aria-label="Search"
+          onChange={event => setKeyword(event.target.value)}
+          onFocus={onFocus}
+        />
+      )}
       <StyledIcon onClick={()=>refine(keyword)} />
     </StyledForm>
 );
