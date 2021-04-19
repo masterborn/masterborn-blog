ca11feee975bdf9dd68bdd1a1d2eaeb5b196eec4
Michał Lach <kontakt@michal-lach.pl>
2021-04-19 15:52:55 +0200

    Scroll up button component

---
+++ src/components/BackToTop.js
@@ -0,0 +1,77 @@
+
+import React, { useState, useEffect, useCallback } from 'react';
+import styled from '@emotion/styled';
+import throttle from 'lodash/throttle';
+import { ChevronUp } from '@styled-icons/fa-solid';
+import { useTransition, animated } from 'react-spring';
+
+const BackTopContainer = styled.div`
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  animation: fadeIn 700ms ease-in-out 1s both;
+  cursor: pointer;
+  z-index: 12;
+  width: 4.6rem;
+  height: 4.6rem;
+  border-radius: 50rem;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  background: ${props => props.theme.colors.primary};
+`
+
+const AnimatedWrapper = animated(BackTopContainer);
+
+const StyledIcon = styled(ChevronUp)`
+  fill: ${props => props.theme.colors.black};
+`
+
+const BackToTop = () =>{
+  const [showScroll, setShowScroll] = useState(false);
+  const transitions = useTransition(
+    showScroll,
+    null,
+    {
+      from: { opacity: 0, transform: 'translateY(-5vh)' },
+      enter: { opacity: 1, transform: 'translateY(0)' },
+      leave: { opacity: 0, transform: 'translateY(-5vh)' },
+      config: { tension: 60, friction: 5, mass: 0.55 },
+    }
+  );
+
+  const checkScrollTop = () => {
+    if (window.pageYOffset > 400){
+      setShowScroll(true);
+    } else if (window.pageYOffset <= 400){
+      setShowScroll(false);
+    }
+  };
+
+  const throttleScroll = useCallback(throttle(checkScrollTop, 300), [showScroll]);
+
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', throttleScroll);
+    return () => window.removeEventListener('scroll', throttleScroll);
+  }, []);
+
+  return (
+    transitions.map(({ item, key, props }) => item && (
+      <AnimatedWrapper
+        key={key}
+        style={{ transform: props.transform, opacity: props.opacity }}
+        onClick={scrollTop}
+      >
+        <StyledIcon size="11" />
+      </AnimatedWrapper>
+  )));
+}
+
+export default BackToTop;
