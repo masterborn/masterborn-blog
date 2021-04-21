d09e8f00344331d57fb858d22fa007725823f0e3
Jakub Bosak <jBosak98@gmail.com>
2021-04-21 13:51:58 +0200

    blogPostsContent refactor

---
+++ src/components/blog/BlogPostsContent.js
@@ -0,0 +1,85 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import ReactPaginate from 'react-paginate';
+
+import { media } from '../../utils/emotion';
+
+import BlogContent from './BlogContent';
+import PostsTiles from './PostsTiles';
+
+const Container = styled(BlogContent)`
+background-color: ${props => props.theme.colors.blogTextBackground};
+padding-bottom: 0;
+${media.desktop`
+  padding-bottom: 0;
+  padding-top: 10rem;
+  width: 129rem;
+  max-width: 129rem;
+`};
+
+.pagination {
+  display: grid;
+  grid-auto-flow: column;
+  width: max-content;
+  margin: auto;
+  opacity: 0.9;
+  font-family: sans-serif;
+  margin-bottom: 9rem;
+  li {
+    width: 3rem;
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.pagination.available};
+  }
+  .active { 
+    color: ${({ theme }) => theme.colors.pagination.active};
+  }
+  .disabled {
+    color: ${({ theme }) => theme.colors.pagination.disabled};
+    cursor: auto;
+  }
+
+  .previous, .next {
+    width: 10rem;
+  }
+}
+`;
+
+const BlogPostsContent = ({postsPerPage, offset, posts, setOffset })=> {
+  const pageCount = Math.ceil(posts.length / postsPerPage);
+  const onPageChange = ({ selected }) => setOffset(Math.ceil(selected * postsPerPage));
+  const paginatedPosts = posts.slice(offset, offset + postsPerPage);
+  
+  return (
+    <Container>
+      <PostsTiles posts={paginatedPosts} />
+      <ReactPaginate
+        previousLabel="&#8592; Newer"
+        nextLabel="Older &#8594;"
+        breakLabel="..."
+        breakClassName="break-me"
+        pageCount={pageCount}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={5}
+        onPageChange={onPageChange}
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+    </Container>
+)
+}
+
+BlogPostsContent.propTypes = {
+    posts: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+    setOffset: PropTypes.func.isRequired,
+    postsPerPage: PropTypes.number,
+    offset: PropTypes.number,
+};
+
+BlogPostsContent.defaultProps = {
+    postsPerPage: 12,
+    offset: 0,
+};
+
+export default BlogPostsContent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { InstantSearch } from "react-instantsearch-dom";
 import partition from 'lodash/partition';
-import ReactPaginate from 'react-paginate';
 
 import useAlgoliaSearch from '../hooks/useAlgoliaSearch';
 import useAllBlogPosts from '../hooks/useAllBlogPosts';
@@ -11,8 +10,7 @@ import BlogContent from '../components/blog/BlogContent';
 import { media } from '../utils/emotion';
 import SEO from '../components/SEO';
 import Search from '../components/Search';
-import PostsTiles from '../components/blog/PostsTiles';
-
+import BlogPostsContent from '../components/blog/BlogPostsContent';
 
 
 const Wrapper = styled('div')`
@@ -41,42 +39,7 @@ const SearchContainer = styled(BlogContent)`
   `}
 `
 
-const BlogPostsContent = styled(BlogContent)`
-  background-color: ${props => props.theme.colors.blogTextBackground};
-  padding-bottom: 0;
-  ${media.desktop`
-    padding-bottom: 0;
-    padding-top: 10rem;
-    width: 129rem;
-    max-width: 129rem;
-  `};
 
-  .pagination {
-    display: grid;
-    grid-auto-flow: column;
-    width: max-content;
-    margin: auto;
-    opacity: 0.9;
-    font-family: sans-serif;
-    margin-bottom: 9rem;
-    li {
-      width: 3rem;
-      cursor: pointer;
-      color: ${({ theme }) => theme.colors.pagination.available};
-    }
-    .active { 
-      color: ${({ theme }) => theme.colors.pagination.active};
-    }
-    .disabled {
-      color: ${({ theme }) => theme.colors.pagination.disabled};
-      cursor: auto;
-    }
-
-    .previous, .next {
-      width: 10rem;
-    }
-  }
-`;
 
 
 
@@ -87,7 +50,7 @@ const Index = () => {
   const [offset, setOffset] = useState(0);
   
   const [featurePosts, restPosts] = partition(posts, ({ isFeature }) => !!isFeature);
-  const pageCount = Math.ceil(restPosts.length / postsPerPage);
+  
   return (
     <InstantSearch
       searchClient={searchClient}
@@ -107,21 +70,12 @@ const Index = () => {
             <FeaturePost key={featurePost.id} post={featurePost} />
           ))}
         </BlogFeatureArticleContent>
-        <BlogPostsContent>
-          <PostsTiles posts={restPosts.slice(offset, offset + postsPerPage)} />
-          <ReactPaginate
-            previousLabel="&#8592; Newer"
-            nextLabel="Older &#8594;"
-            breakLabel="..."
-            breakClassName="break-me"
-            pageCount={pageCount}
-            marginPagesDisplayed={1}
-            pageRangeDisplayed={5}
-            onPageChange={({ selected }) => setOffset(Math.ceil(selected * postsPerPage))}
-            containerClassName="pagination"
-            activeClassName="active"
-          />
-        </BlogPostsContent>
+        <BlogPostsContent 
+          postsPerPage={postsPerPage} 
+          offset={offset} 
+          setOffset={setOffset} 
+          posts={restPosts}
+        />
       </Wrapper>
     </InstantSearch>
   );
