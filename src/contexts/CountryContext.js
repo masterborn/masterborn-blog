d672a8748d1ad6819756d5386fc110c2e28b1787
Jakub Bosak <jBosak98@gmail.com>
2021-04-26 14:48:56 +0200

    contryContext value changed back to state

--- src/contexts/CountryContext.js
+++ src/contexts/CountryContext.js
@@ -15,7 +15,7 @@ export const CountryContextProvider = (props) => {
   return (
     <CountryContext.Provider
       value={{
-        isInPoland: false,
+        isInPoland: state,
         setCountry,
       }}
     >
